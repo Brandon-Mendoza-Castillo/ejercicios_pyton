328da7bc7909f4c4924ed81816013b11efd7487f
Brandon-Mendoza-Castillo <160100654+Brandon-Mendoza-Castillo@users.noreply.github.com>
2024-09-22 23:21:05 -0600

    Create Ejercicio_26.py

---
+++ Ejercicio_26.py
@@ -0,0 +1,7 @@
+numero = float(input("Ingresa un número: "))
+if numero > 0:
+    print(f"{numero} es un número positivo.")
+elif numero < 0:
+    print(f"{numero} es un número negativo.")
+else:
+    print("El número es cero.")
