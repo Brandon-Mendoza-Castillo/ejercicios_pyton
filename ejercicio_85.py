e16ed7eec79deadcbea047bcf34ea12429e3be2a
Brandon-Mendoza-Castillo <160100654+Brandon-Mendoza-Castillo@users.noreply.github.com>
2024-10-25 23:07:02 -0600

    Add files via upload

---
+++ ejercicio_85.py
@@ -0,0 +1,10 @@
+
+import random
+import string
+
+def generar_contrasena(longitud=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
+    return contrasena
+
+print(generar_contrasena())
